a413eaa9a3166cc7b1598868b4a7324b2fb1b3e3
kanehooper <kanehooper@hotmail.com>
2019-02-06 22:40:04 +1100

    MongoDB update function

---
+++ playground/mongodb-update.js
@@ -0,0 +1,48 @@
+console.clear();
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        console.log('Could not connect to the database');
+    }
+    console.log('Connected to the MongoDB server');
+    var db = client.db('TodoApp');
+
+    // // findOneAndUpdate
+    // db.collection('Todos')
+    //     .findOneAndUpdate({
+    //         _id: new ObjectID('5c597838cc7d93077e8dde11')
+    //     }, {
+    //         $set: {
+    //             completed: true
+    //         }
+    //     }, {
+    //         returnOriginal: false
+    //     })
+    //     .then((result) => {
+    //         console.log(JSON.stringify(result, undefined, 2));
+    //     })
+
+    // client.close();
+    db.collection('users')
+        .findOneAndUpdate({
+            _id: new ObjectID('5c5a9eaa8c5d161a4ed9a955')
+        }, {
+            $set: {
+                name: 'Jimbo'
+            },
+            $inc: {
+               age: 1 
+            }
+        }, {
+            returnOriginal: false
+        })
+        .then((result) => {
+            console.log(JSON.stringify(result, undefined, 2));
+        })
+
+})
+
+// users
+// Update the name
+// $inc to increement the age by one
