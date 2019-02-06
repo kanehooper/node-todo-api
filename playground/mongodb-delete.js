ad3465b942e0cff737db806c03edb8b780fa72ef
kanehooper <kanehooper@hotmail.com>
2019-02-06 21:55:32 +1100

    MongoDB delete function

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,44 @@
+ console.clear();
+
+ const {MongoClient, ObjectID} = require('mongodb');
+
+ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+     if (err) {
+         console.log('Cannot connect to the database', err);
+     }
+     console.log('Connected to MongoDB server');
+     var db = client.db('TodoApp');
+
+     // deleteMany
+    //  db.collection('Todos')
+    //     .deleteMany({
+    //         text: 'Eat lunch'
+    //     })
+    //     .then((result) => {
+    //         console.log(result);
+    //     }
+    // );
+
+     // deleteOne
+    //  db.collection('Todos')
+    //     .deleteOne({
+    //         text: 'Eat lunch'
+    //     })
+    //     .then((result) => {
+    //         console.log(result);
+    //     }
+    // );
+        
+    // findOneAndDelete
+    db.collection('Todos')
+        .findOneAndDelete({
+            completed: false
+        })
+        .then((result) => {
+            console.log(result);
+        }, (err) => {
+            console.log(err);
+        })
+
+    //  client.close();
+ })
